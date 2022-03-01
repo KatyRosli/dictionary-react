df69b5b90262fb543f9ce5bc9b849ff9e2ee3027
KatyRosli <96566968+KatyRosli@users.noreply.github.com>
2022-03-01 18:19:31 +0100

    set up dictionary app react

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Dictionary App">
+        <p>Hello Dictionary app here</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
