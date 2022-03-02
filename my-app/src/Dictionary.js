ef1450a585331e6f27227a87e3b4b57088250562
KatyRosli <96566968+KatyRosli@users.noreply.github.com>
2022-03-02 20:35:32 +0100

    update search function

--- my-app/src/Dictionary.js
+++ my-app/src/Dictionary.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} defination`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
   return (
     <div className="Dictionary">
       <form className="form-inline">
         <input
+          onSubmit={search}
           className="form-control"
           type="search"
-          placeholder="Search for a city..."
+          placeholder="Search for a word..."
           autoFocus="on"
           aria-label="Search"
+          onChange={handleKeywordChange}
         />
         <button type="submit" value="Search" className="btn btn-warning">
           <i className="fa-solid fa-magnifying-glass"></i>
