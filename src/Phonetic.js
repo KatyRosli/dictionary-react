4cac68d2f0f48c3d12406034957a09634379985d
KatyRosli <96566968+KatyRosli@users.noreply.github.com>
2022-03-05 10:21:36 +0100

    edited phonetic component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
+  const [play, setPlay] = useState(false);
+
+  function handleClick() {
+    setPlay(true);
+  }
+
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
-        Listen
-      </a>
-      <br />
-      {props.phonetic.text}
+      <span className="phonetic">{props.phonetic.audio}</span>
+      <button onClick={handleClick}>
+        <i className="fas fa-volume-up"></i>
+      </button>
+      {play && (
+        <ReactAudioPlayer
+          src={props.phonetic.audio}
+          autoPlay
+          onEnded={() => setPlay(false)}
+        />
+      )}
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -6,19 +6,25 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
+        <section>
+          <h1 className="word">{props.results.word}</h1>
+          {props.results.phonetics.map(function (phonetic, index) {
+            if (phonetic.audio) {
+              return (
+                <div key={index}>
+                  <Phonetic phonetic={phonetic} />
+                </div>
+              );
+            }
+          })}
+        </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
+            <section>
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            </section>
           );
         })}
       </div>
